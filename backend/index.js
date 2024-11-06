88b1aec14237a60162e4a163055c6a59d11ad277
Jatin Malik <kumar.jatinitp2019@gmail.com>
2024-11-06 18:12:27 +0530

    all changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
  const connectDB = require('./config/db.js'); // Import the connectDB function
 require('dotenv').config(); // Load environment variables
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2000;
 const authRoutes = require('./routes/auth.js');
 const cors = require('cors');
 // Connect to MongoDB
@@ -16,13 +16,11 @@ const cors = require('cors');
 
 // Routes
 // Middleware
-app.use(cors(
-    {
-        origin: ["https://cricslot.vercel.app/"],
-        methods:["POST", "PATCH", "PUT", "DELETE", "GET"],
-        credentials: true
-    }
-)); // Enable CORS for all requests
+app.use(cors({
+    origin: ["https://cricslot.vercel.app/api/", "http://localhost:3000"], // Allow both production and local origins
+    methods: ["POST", "PATCH", "PUT", "DELETE", "GET"],
+    credentials: true
+})); // Enable CORS for all requests
 app.use(express.json()); // Parse incoming JSON requests
 app.use('/api/auth', authRoutes); // Authentication routes
 
